5e9bc6b422cee92b3143965bcb099a44b4ff1811
Davit Bakhshyan <a1l2o3@bk.ru>
2026-05-10 16:51:16 +0400

    game logic

--- Breakout/include/game.h
+++ Breakout/include/game.h
@@ -4,11 +4,28 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#include "game_level.h"
+
 enum GameState {
     GAME_ACTIVE,
     GAME_MENU,
     GAME_WIN
 };
+
+enum Direction {
+    UP,
+    RIGHT,
+    DOWN,
+    LEFT
+};
+
+typedef std::tuple<bool, Direction, glm::vec2> Collision;
+
+const glm::vec2 PLAYER_SIZE(100.0f, 20.0f);
+const float PLAYER_VELOCITY(500.0f);
+
+const glm::vec2 INITIAL_BALL_VELOCITY(100.0f, -350.0f);
+const float BALL_RADIUS = 8.0f;
 
 class Game
 {
@@ -16,6 +33,9 @@ public:
     GameState               State;
     bool                    Keys[1024];
     unsigned int            Width, Height;
+
+    std::vector<GameLevel>  Levels;
+    unsigned int            Level;
 
     Game(unsigned int width, unsigned int height);
     ~Game();
@@ -25,6 +45,10 @@ public:
     void ProcessInput(float dt);
     void Update(float dt);
     void Render();
+    void DoCollisions();
+
+    void ResetLevel();
+    void ResetPlayer();
 };
 
 #endif
